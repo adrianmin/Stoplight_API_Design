6ee032aaaffca0507266bc6f07ad510e766f1503
adrian.minnock@smartbear.com <adrian.minnock@smartbear.com>
2024-08-27 16:11:17 +0100

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1Njg0NQ/spectral.js?branch_slug=main&token=4f082947-2396-47ac-b8b7-136aee87bb86";
+export default { extends: ruleset };
